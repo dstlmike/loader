df0d2e1985797d6bcc03f825e0b8759b3b1ad203
dstlmike <dstl_mike1@hotmail.com>
2021-02-28 21:38:28 -0500

    Update model.js

--- model.js
+++ model.js
@@ -1,7 +1,6 @@
 // Step 3 - this is the code for ./models.js
 
 var mongoose = require('mongoose');
-var Image = new mongoose.model('Image', imageSchema, 'uploads');
 
 var imageSchema = new mongoose.Schema({
     name: String,
@@ -12,6 +11,7 @@ var imageSchema = new mongoose.Schema({
         contentType: String
     }
 });
+var Image = new mongoose.model('Image', imageSchema, 'uploads');
 
 //Image is a model which has a schema imageSchema
 
