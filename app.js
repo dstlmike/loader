0dcbd817acfe0a7b3124657345d491660fec4b9c
dstlmike <dstl_mike1@hotmail.com>
2021-03-02 23:16:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -30,14 +30,15 @@ app.set("view engine", "ejs");
 
 var multer = require('multer');
 
-var storage = multer.memoryStorage();  // multer.diskStorage({
-//    destination: (req, file, cb) => {
-     //   cb(null, 'uploads')
- //   },
- //   filename: (req, file, cb) => {
-     //   cb(null, file.fieldname + '-' + Date.now())
- //   }
-//});
+//var storage = multer.memoryStorage();  // 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
 
 var upload = multer({ storage: storage });
 
