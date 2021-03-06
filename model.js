05eadbd6448ffaa42011a8177cd3a488203632d1
dstlmike <dstl_mike1@hotmail.com>
2021-03-06 16:59:10 -0500

    Update model.js

--- model.js
+++ model.js
@@ -16,4 +16,4 @@ var imageSchema = new mongoose.Schema({
 
 //Image is a model which has a schema imageSchema
 
-module.exports = new mongoose.model('uploads', imageSchema);
+module.exports = new mongoose.model('image', imageSchema);
